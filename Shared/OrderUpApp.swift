43a230d99776df856200178efc14a89a07b1dbb8
Mohammad Mohammad hemanimani <mohammadhemani@gmail.com>
2021-08-27 03:00:35 +0200

    Initial Commit

---
+++ Shared/OrderUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderUpApp.swift
+//  Shared
+//
+//  Created by user205179 on 8/27/21.
+//
+
+import SwiftUI
+
+@main
+struct OrderUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
